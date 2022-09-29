2e8ea55fbf5a9443cf6e45f6aabf3f400a08e4db
Nakul dev Khajuria <nakuldevkhajuria@gmail.com>
2022-09-29 19:04:25 +0530

    error resolved for use jsx under scope

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ClassComponent from "./components/classComponent"
-
+import React from "react";
 import "./App.css";
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
     
     <div className="App">
       <p className="header">Employee Feedback Form</p>
+
       <ClassComponent id="class"/>
 
 
